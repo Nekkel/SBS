709f243c059b7cd0d1c6109c71e06b432a9fe51e
Nekkel <156950312+Nekkel@users.noreply.github.com>
2024-01-22 21:30:39 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import webbrowser
 
 try:
     from googlesearch import search
@@ -9,6 +10,8 @@ query = "java википедия"
 
 for j in search(query, tld="co.in", num=1, stop=1, pause=2):
 
+    webbrowser.open('https://ru.wikipedia.org/wiki/Java', new=2)
+
     print(j)
 
 # to search2
@@ -17,9 +20,16 @@ query = "python википедия"
 
 for k in search(query, tld="co.in", num=1, stop=1, pause=2):
 
+    webbrowser.open('https://ru.wikipedia.org/wiki/Python', new=2)
+
     print(k)
+
 # to search 3
+
     query = "хентай википедия"
 
     for l in search(query, tld="co.in", num=1, stop=1, pause=2):
+
+        webbrowser.open('https://ru.wikipedia.org/wiki/%D0%A5%D0%B5%D0%BD%D1%82%D0%B0%D0%B9', new=2)
+
         print(l)
